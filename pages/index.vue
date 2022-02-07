d04e986a3fec956d2bc6b739b6cb3e95705d5ce6
Georgi Kirilov <gkkirilov@gmail.com>
2022-02-07 04:02:32 +0200

    Update homepage #3

--- pages/index.vue
+++ pages/index.vue
@@ -25,7 +25,7 @@
       <div class="row text-center content d-flex justify-content-center">
         <div class="col-12 col-md-6 mb-5 pb-5">
           <h1 class="text-white display-1 ml-3 mt-4">We support companies who create products that matter.</h1>
-          <div class="display-4">Email us: <a href="mailto:georgi@kirilovconsult.com">georgi@kirilovconsult.com</div>
+          <div class="display-4"><a class="text-white" href="mailto:georgi@kirilovconsult.com">georgi@kirilovconsult.com</div>
         </div>
       </div>
     </div>
