ee39923a44b690079b2fa9b921b1d546911ede5b
Georgi Kirilov <gkkirilov@gmail.com>
2022-11-28 14:54:22 +0200

    Update site

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,8 @@
       style="background-image: url('img/landing.jpg');"
     >
       <div style="position: relative" class="ml-md-5 row pt-5">
-        <div
-          class="col-md-5 col-12 text-center display-2 display-md-2 d-flex"
-        >
-            <!-- src="/logo/vector/default-monochrome.svg" -->
+        <div class="col-md-5 col-12 text-center display-2 display-md-2 d-flex">
+          <!-- src="/logo/vector/default-monochrome.svg" -->
           <img
             src="/img/logo.png"
             class="navbar-brand-img d-none d-sm-block logo"
@@ -24,11 +22,53 @@
 
       <div class="row text-center content d-flex justify-content-center">
         <div class="col-6 col-md-6 mb-5 pb-5">
-          <h1 class="text-white display-1 ml-3 mt-4">Supporting companies and products that matter.</h1>
-          <div class="display-4"><a class="text-white" href="mailto:georgi@kirilovconsult.com">georgi@kirilovconsult.com</div>
-        </div>
-        <div class="col-6 col-md-6 mb-5 pb-5">
-          <iframe src="https://v1.embednotion.com/embed/260cc3ce2a1240c59a1c0c5919dd2c36"></iframe>  <style>  iframe { width: 100%; height: 500px; border: 2px solid #ccc; border-radius: 10px; padding: none; }  </style>
+          <div class="row">
+            <div class="col-12">
+              <h1 class="text-white display-1 ml-3 mt-4">
+                Supporting companies and products that matter.
+              </h1>
+              <div class="display-4">
+                <a class="text-white" href="mailto:georgi@kirilovconsult.com"
+                  >georgi@kirilovconsult.com
+                </a>
+              </div>
+            </div>
+            <div class="col-md-6 mt-5">
+              <div class="card">
+                <img src="" class="card-img-top" alt="" />
+                <div class="card-body">
+                  <h5 class="card-title">Consultant work</h5>
+                  <p class="card-text">
+                    Help you build and ideate on the best solution for your e-commerce project running on Salesforce Commerce Cloud.
+                  </p>
+                  <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6 mt-5">
+              <div class="card">
+                <img src="" class="card-img-top" alt="" />
+                <div class="card-body">
+                  <h5 class="card-title">Build Cartidges</h5>
+                  <p class="card-text">
+                    Support you in building and certifying your Salesforce Commerce Cloud cartridge.
+                  </p>
+                  <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 mt-5 p-0">
+            <div class="card">
+                <div class="card-body">
+                  <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                  <Cv/>
+                </div>
+              </div>
+            <!-- <iframe
+              src="https://v1.embednotion.com/embed/260cc3ce2a1240c59a1c0c5919dd2c36"
+            ></iframe> -->
+          </div>
         </div>
       </div>
     </div>
@@ -36,58 +76,64 @@
 </template>
 
 <script>
-
+import Cv from '~/components/cv.vue';
 export default {
-  layout(context) {
-    return 'header';
-  },
-  data() {
-    return {
-      email: ''
-    };
-  },
-  computed: {
-    deadlineDate: () => {
-      var deadline = new Date();
-
-      if (deadline.getMonth() == 11) {
-        deadline.setYear(deadline.getYear() + 1)
-        deadline.setMonth(0)
-      } else {
-        deadline.setMonth(deadline.getMonth() + 1)
-      }
-      return deadline.toDateString()
-    }
-  },
-  methods: {
-    validateEmail(email) {
-      var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(email);
+    layout(context) {
+        return "header";
     },
-    subscribe(e) {
-      e.preventDefault();
-
-      if (!this.validateEmail(this.email)) {
-        this.$toast.error('Please enter correct email 💥');
-        return;
-      }
-
-      this.$axios.post('https://formspree.io/mvorogja', {
-        _replyto: this.email
-      });
-
-      this.$toast.success('Successfully subscribed! 📬');
-    }
-  }
+    data() {
+        return {
+            email: ""
+        };
+    },
+    computed: {
+        deadlineDate: () => {
+            var deadline = new Date();
+            if (deadline.getMonth() == 11) {
+                deadline.setYear(deadline.getYear() + 1);
+                deadline.setMonth(0);
+            }
+            else {
+                deadline.setMonth(deadline.getMonth() + 1);
+            }
+            return deadline.toDateString();
+        }
+    },
+    methods: {
+        validateEmail(email) {
+            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);
+        },
+        subscribe(e) {
+            e.preventDefault();
+            if (!this.validateEmail(this.email)) {
+                this.$toast.error("Please enter correct email 💥");
+                return;
+            }
+            this.$axios.post("https://formspree.io/mvorogja", {
+                _replyto: this.email
+            });
+            this.$toast.success("Successfully subscribed! 📬");
+        }
+    },
+    components: { Cv }
 };
 </script>
 
 <style lang="scss">
+iframe {
+  width: 100%;
+  min-height: 1000px;
+  max-height: 100vh;
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  padding: none;
+}
 .content {
-  margin-top: 250px;
+  margin-top: 50px;
 
   @media only screen and (max-width: 1200px) {
-    margin-top: 150px;
+    margin-top: 50px;
   }
   @media only screen and (max-width: 768px) {
     margin-top: 70px;
@@ -112,7 +158,7 @@ export default {
 }
 
 .overlay1::before {
-  content: '';
+  content: "";
   display: block;
   position: absolute;
   z-index: -1;
